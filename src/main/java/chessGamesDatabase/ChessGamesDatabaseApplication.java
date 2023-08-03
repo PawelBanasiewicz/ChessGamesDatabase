641d4df766d9568c8f29790513a500e125fbe98a
PawelBanasiewicz <pawel.j.banasiewicz@gmail.com>
2023-08-03 21:28:30 +0200

    initial project

---
+++ src/main/java/chessGamesDatabase/ChessGamesDatabaseApplication.java
@@ -0,0 +1,13 @@
+package chessGamesDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessGamesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessGamesDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chessGamesDatabase/ChessGamesDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package chessGamesDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessGamesDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
