e41c45a59e22338dacf40226187f4f4745212412
PawelBanasiewicz <pawel.j.banasiewicz@gmail.com>
2023-08-13 08:34:17 +0200

    add passwordEncoder Bean to SecurityConfig (users passwords are no longer store with "{bcrypt}" prefix

--- src/main/java/chessGamesDatabase/security/SecurityConfig.java
+++ src/main/java/chessGamesDatabase/security/SecurityConfig.java
@@ -4,16 +4,21 @@ package chessGamesDatabase.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig  {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
